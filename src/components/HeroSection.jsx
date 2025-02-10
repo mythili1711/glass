96f943f57b1e6c054af9c370f363b94617fb1238
C.MYTHILI <148332849+mythili2126j0525@users.noreply.github.com>
2025-02-10 12:11:07 +0530

    hero section started

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -2,9 +2,17 @@ import React from 'react'
 
 const HeroSection = () => {
   return (
-  <section className='min-h-screen'>nhjjjfj</section>
-  
-  )
-}
+ <section className='min-h-screen relative'>
+    <img src="/Rectangle 6.png" alt='bg-hero' className='absolute w-full h-full object-cover'/>
+  <div className="flex items-center justify-center border min-h-screen">
+  <div className='flex flex-col gap y-2'>
+    <h2 className='text-[#87C159] font-semibold text-6xl'>EYE GLASS</h2>
+    <p className='font-thin'>Look On The Bright Side Of Life</p>
+<button className='px-3 py-1 bg-neutral-800 rounded w-fit'>Shop Now</button>
+  </div>
+  </div>
+ </section>
+  );
+};
 
 export default HeroSection
